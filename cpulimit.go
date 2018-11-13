4ee5e34f432635ecd57991092311991967f06927
Marcel Gebhardt <marcel.gebhardt@bsk-consulting.de>
2018-11-13 16:36:04 +0100

    using average of last 4 measurements

--- cpulimit.go
+++ cpulimit.go
@@ -7,34 +7,19 @@ import (
 	"github.com/shirou/gopsutil/cpu"
 )
 
-func getBusy() (busy, all float64) {
-	ts, err := cpu.Times(false)
-	if err != nil {
-		panic(err)
-	}
-	t := ts[0]
-	busy = t.User + t.System + t.Nice + t.Iowait + t.Irq + t.Softirq + t.Steal + t.Guest + t.GuestNice + t.Stolen
-	all = busy + t.Idle
-	return
-}
-
-func getCPUUsage(busy1, all1, busy2, all2 float64) float64 {
-	if all1 == all2 {
-		return 0.0
-	}
-	usage := ((busy2 - busy1) / (all2 - all1)) * 100.0
-	return usage
-}
+const defaultLimit float64 = 80.0
 
 type Limiter struct {
-	MaxCPUUsage     float64
-	MeasureInterval time.Duration
-	stop            bool
-	wg              *sync.WaitGroup
-	mutex           *sync.RWMutex
+	MaxCPUUsage float64
+	stop        bool
+	wg          *sync.WaitGroup
+	mutex       *sync.RWMutex
 }
 
 func (l *Limiter) Start() {
+	if l.MaxCPUUsage == 0.0 {
+		l.MaxCPUUsage = defaultLimit
+	}
 	l.wg = &sync.WaitGroup{}
 	l.mutex = &sync.RWMutex{}
 	l.wg.Add(1)
@@ -60,10 +45,12 @@ func (l *Limiter) run() {
 		all2     float64
 		cpuUsage float64
 		locked   bool
+		m        = make([]float64, 4) // measurements
 	)
-	tk := time.NewTicker(l.MeasureInterval)
+	tk := time.NewTicker(time.Millisecond * 500)
 	defer tk.Stop()
 	busy2, all2 = getBusy()
+	var counter int
 	for range tk.C {
 		if l.stop {
 			if locked {
@@ -75,7 +62,8 @@ func (l *Limiter) run() {
 		busy1, all1 = busy2, all2
 		busy2, all2 = getBusy()
 		cpuUsage = getCPUUsage(busy1, all1, busy2, all2)
-		if cpuUsage > l.MaxCPUUsage {
+		m[counter] = cpuUsage
+		if (m[0]+m[1]+m[2]+m[3])/4.0 > l.MaxCPUUsage {
 			if !locked {
 				l.mutex.Lock()
 				locked = true
@@ -86,5 +74,28 @@ func (l *Limiter) run() {
 				locked = false
 			}
 		}
+		counter++
+		if counter > 3 {
+			counter = 0
+		}
 	}
+}
+
+func getBusy() (busy, all float64) {
+	ts, err := cpu.Times(false)
+	if err != nil {
+		panic(err)
+	}
+	t := ts[0]
+	busy = t.User + t.System + t.Nice + t.Iowait + t.Irq + t.Softirq + t.Steal + t.Guest + t.GuestNice + t.Stolen
+	all = busy + t.Idle
+	return
+}
+
+func getCPUUsage(busy1, all1, busy2, all2 float64) float64 {
+	if all1 == all2 {
+		return 0.0
+	}
+	usage := ((busy2 - busy1) / (all2 - all1)) * 100.0
+	return usage
 }
